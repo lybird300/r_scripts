cc079144898aebf5c7ddd3236cc9cef684bd5105
macs82 <macs82@gmail.com>
2015-05-21 19:03:57 +0100

    modified scripts

--- col_pop.r
+++ col_pop.r
@@ -7,14 +7,26 @@ col_pop <- function(pops){
   CAR_s_col = "#F99595"
   CAR_n_col = "#FBB8B8"
   CEU_col = "#13256F"
-  Erto_col = "#10DFBC"
   FVG_col ="#505E69"
   FVG_p_col ="#737E87"
   FVG_s_col ="#969EA5"
   FVG_n_col ="#B9BFC3"
+  Erto_col = "#10DFBC"
   Illegio_col = "#48867F"
   Resia_col = "#16C224"
   Sauris_col = "#00631F"
+  Erto_n_col = "#10DFBC"
+  Illegio_n_col = "#48867F"
+  Resia_n_col = "#16C224"
+  Sauris_n_col = "#00631F"
+  Erto_p_col = "#10DFBC"
+  Illegio_p_col = "#48867F"
+  Resia_p_col = "#16C224"
+  Sauris_p_col = "#00631F"
+  Erto_s_col = "#10DFBC"
+  Illegio_s_col = "#48867F"
+  Resia_s_col = "#16C224"
+  Sauris_s_col = "#00631F"
   TSI_col = "#619FE0"
   VBI_col = "#CA8A1A"
   VBI_p_col = "#D5A148"
@@ -62,21 +74,69 @@ col_pop <- function(pops){
     cur_col <- FVG_n_col
     density <- 1
   }
-  if(pops[i] == "Erto" || pops[i] == "FVE" || pops[i] == "FVG-E"){
+  if(pops[i] == "Erto" || pops[i] == "FVE" || pops[i] == "FVG-E" ){
     cur_col <- Erto_col
     density <- 8
   }
-  if(pops[i] == "Illegio" || pops[i] == "FVI" || pops[i] == "FVG-I"){
+  if(pops[i] == "Illegio" || pops[i] == "FVI" || pops[i] == "FVG-I" ){
     cur_col <- Illegio_col
     density <- 8
   }
-  if(pops[i] == "Resia" || pops[i] == "FVR" || pops[i] == "FVG-R"){
+  if(pops[i] == "Resia" || pops[i] == "FVR" || pops[i] == "FVG-R" ){
     cur_col <- Resia_col
     density <- 8
   }
-  if(pops[i] == "Sauris" || pops[i] == "FVS" || pops[i] == "FVG-S"){
+  if(pops[i] == "Sauris" || pops[i] == "FVS" || pops[i] == "FVG-S" ){
     cur_col <- Sauris_col
     density <- 8
+  }
+  if(pops[i] == "Erto_n" || pops[i] == "Erto_s" || pops[i] == "Erto_p"){
+    cur_col <- Erto_col
+    density <- 1
+  }
+  if(pops[i] == "Illegio_n" || pops[i] == "Illegio_s" || pops[i] == "Illegio_p"){
+    cur_col <- Illegio_col
+    density <- 1
+  }
+  if(pops[i] == "Resia_n" || pops[i] == "Resia_s" || pops[i] == "Resia_p"){
+    cur_col <- Resia_col
+    density <- 1
+  }
+  if(pops[i] == "Sauris_n" || pops[i] == "Sauris_s" || pops[i] == "Sauris_p"){
+    cur_col <- Sauris_col
+    density <- 1
+  }
+  if(pops[i] == "Erto_s" || pops[i] == "Erto_p"){
+    cur_col <- Erto_col
+    density <- 2
+  }
+  if(pops[i] == "Illegio_s" || pops[i] == "Illegio_p"){
+    cur_col <- Illegio_col
+    density <- 2
+  }
+  if(pops[i] == "Resia_s" || pops[i] == "Resia_p"){
+    cur_col <- Resia_col
+    density <- 2
+  }
+  if(pops[i] == "Sauris_s" || pops[i] == "Sauris_p"){
+    cur_col <- Sauris_col
+    density <- 2
+  }
+  if( pops[i] == "Erto_p"){
+    cur_col <- Erto_col
+    density <- 3
+  }
+if( pops[i] == "Illegio_p"){
+    cur_col <- Illegio_col
+    density <- 3
+  }
+if( pops[i] == "Resia_p"){
+    cur_col <- Resia_col
+    density <- 3
+  }
+if( pops[i] == "Sauris_p"){
+    cur_col <- Sauris_col
+    density <- 3
   }
   if(pops[i] == "VBI"){
     cur_col <- VBI_col
