4dc1221ac9cbf6ae78dff2ae973585314603dee8
macsx82 <macsx82@gmail.com>
2016-05-17 08:39:38 +0200

    modified INGI_ref_checks_CONCO to fix reading file error

--- INGI_ref_checks_CONCORDANCE.r
+++ INGI_ref_checks_CONCORDANCE.r
@@ -57,8 +57,8 @@ for (pop in pops){
             current_pop_current_panel_current_chr_info_name <- paste(pop_folder,"/",chr,"/chr",chr,".gen_info_partial_t2.gz",sep="")
             # current_pop_current_panel_current_chr_info <-  read.table(current_pop_current_panel_current_chr_info_name,header=T,nrows=100000)
             # CHROM RS_ID POS EXP_FREQ_A1 INFO TYPE INFO_TYPE0 CONCORD_TYPE0 r2_TYPE0 COHORT PANEL MAF BIN
-            current_pop_current_panel_current_chr_info <-  read.table(current_pop_current_panel_current_chr_info_name,header=T,sep=" ",stringsAsFactors=F, comment.char="",colClasses=c("integer","character","integer",rep("numeric",2),"character","NULL","NULL","NULL",rep("character",2),rep("numeric",2)))
-            current_pop_all_panels_all_chr_complete <- rbind(current_pop_all_panels_all_chr_complete,current_pop_current_panel_current_chr_info)
+            current_pop_current_panel_current_chr_info <-  read.table(current_pop_current_panel_current_chr_info_name,header=T,sep=" ",stringsAsFactors=F, comment.char="",colClasses=c("integer","character","integer",rep("numeric",2),"character",rep("numeric",3),rep("character",2),rep("numeric",2)))
+            current_pop_all_panels_all_chr <- rbind(current_pop_all_panels_all_chr,current_pop_current_panel_current_chr_info)
             # assign(paste("complete_",pop,"info",sep=""),current_pop_all_panels_all_chr)
         }
     }
